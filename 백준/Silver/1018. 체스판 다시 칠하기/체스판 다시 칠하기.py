289be714f63257035f9a50ef5b53e607c9f4899d
bogyeom0922 <97151951+bogyeom0922@users.noreply.github.com>
2022-08-22 01:12:24 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 120 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -5,22 +5,24 @@ count = []
 for _ in range(N):
     chess.append(input())
 
-for row in range(N-7):
-    for col in range(M-7):
-        index1 = 0
-        index2 = 0
+
+
+for row in range(N - 7):
+    for col in range(M - 7):
+        w_start = 0
+        b_start = 0
         for i in range(row, row + 8):
             for j in range(col, col + 8):
                 if (i + j) % 2 == 0:
                     if chess[i][j] != 'W':
-                        index1 += 1
+                        w_start += 1
                     if chess[i][j] != 'B':
-                        index2 += 1
+                        b_start += 1
                 else:
                     if chess[i][j] != 'B':
-                        index1 += 1
+                        w_start += 1
                     if chess[i][j] != 'W':
-                        index2 += 1
-        count.append(min(index1, index2))
+                        b_start += 1
+        count.append(min(w_start, b_start))
 
 print(min(count))
