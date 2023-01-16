22c966b35f97d659b008d892c9fcfce061a83c9d
bogyeom0922 <97151951+bogyeom0922@users.noreply.github.com>
2023-01-16 23:11:56 +0900

    [Bronze V] Title: A+B - 5, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
@@ -1,9 +1,7 @@
-Sum = []
+import sys
 while True:
-    A, B = map(int, input().split())
-    if A == B == 0:
+    A,B = map(int, sys.stdin.readline().split())
+    if A==0 and B==0:
         break
-    Sum.append(A + B)
-
-for i in range(len(Sum)):
-    print(Sum[i])
+    else:
+        print(A+B)
