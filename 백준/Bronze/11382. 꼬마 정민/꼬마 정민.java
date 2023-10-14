46d697d8826b2876d529005b584458af19615e48
bogyeom <97151951+bogyeom0922@users.noreply.github.com>
2023-10-14 12:15:46 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		long A = scan.nextLong();
+		long B = scan.nextLong();
+		long C = scan.nextLong();
+
+		System.out.print(A+B+C);
+		
+		scan.close();
+		
+	}
+
+}
