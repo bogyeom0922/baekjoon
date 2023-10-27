e0b3c9aaddd16d5d54158dd801d2bd7300471c41
bogyeom <97151951+bogyeom0922@users.noreply.github.com>
2023-10-27 20:41:59 +0900

    [Bronze V] Title: 아스키 코드, Time: 208 ms, Memory: 17604 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+
+		char c = scan.next().charAt(0);
+		System.out.print((int)c);
+		
+		scan.close();
+		
+	}
+
+}
